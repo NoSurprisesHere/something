b5338fb16720ff468d54191fd69cc6271ef20a6e
Вячеслав Мишин <gamerverryeasy@gmail.com>
2019-02-02 14:13:04 +0300

    no message

---
+++ src/by/homefolder/javabook1/exercise/poolpuzzle1/poolpuzzle1.java
@@ -0,0 +1,29 @@
+package by.homefolder.javabook1.exercise.poolpuzzle1;
+
+public class poolpuzzle1 {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<4){
+            System.out.print("a");
+            if (x<1){
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if (x>1){
+                System.out.print(" oyster");
+                x=x+2;
+            }
+            if (x==1){
+                System.out.print("noys");
+            }
+            if (x<1){
+                System.out.print("oise ");
+            }
+            System.out.println("");
+            x=x+1;
+
+
+
+        }
+    }
+}
