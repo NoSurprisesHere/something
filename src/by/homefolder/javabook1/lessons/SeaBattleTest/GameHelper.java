7d8f02d314f8a10027aaa4616954d7c8860eb451
Вячеслав Мишин <gamerverryeasy@gmail.com>
2019-02-05 19:19:17 +0300

    no message

---
+++ src/by/homefolder/javabook1/lessons/SeaBattleTest/GameHelper.java
@@ -0,0 +1,23 @@
+package by.homefolder.javabook1.lessons.SeaBattleTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.management.BufferPoolMXBean;
+
+public class GameHelper {
+    String getUserInput(String usGuesss) {
+        String inputLine = null;
+        System.out.print(usGuesss + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+        }
+        return inputLine;
+
+    }
+}
---
+++ src/by/homefolder/javabook1/lessons/SeaBattleTest/SimpleDotComTestDrive.java
@@ -0,0 +1,38 @@
+package by.homefolder.javabook1.lessons.SeaBattleTest;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        SimpleDotCom dot = new SimpleDotCom();
+        GameHelper enterGuess = new GameHelper();
+        int x, y, z, numOfGuesses;
+        numOfGuesses = 0;
+        x = (int) (Math.random() * 11);
+        y = x + 1;
+        z = y + 1;
+        int[] locations = {x, y, z};
+        dot.setLocationCells(locations);
+        if (dot.numOfHits < locations.length) {
+            boolean isAlive = true;
+            while (isAlive) {
+                String userGuess = enterGuess.getUserInput("Введите число от 0 до 10");
+                String result = dot.checkYourself(userGuess);
+                if (result.equals("Мимо")){
+                    System.out.println("Жаль, пробуй ещё");
+                    numOfGuesses++;
+                }
+                if (result.equals("Попал")){
+                    System.out.println("Попал, стреляй ещё!");
+                    numOfGuesses++;
+                }
+                if (result.equals("Потопил")){
+                    numOfGuesses++;
+                    System.out.println("Ура ты потопил свой массив,1 WOOHOO");
+                    System.out.println("Конец игры :) количество попыток: " + numOfGuesses);
+                    isAlive=false;
+                }
+            }
+
+
+        }
+    }
+}
